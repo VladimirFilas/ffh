20a648cf87a61bd7f009f058edd1fa5bd2bfb100
Vladimir Filas <226385@vutbr.cz>
2022-03-29 09:05:36 +0200

    fdfd

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -1,3 +1,9 @@
+def is_even_number(number):
+    if number %2 ==0:
+        return True
+
+    return False
+
 def is_whole_number(number):
     if number %1 == 0 and number>=0:
         return True
@@ -5,4 +11,7 @@ def is_whole_number(number):
     return False
 
 my_number = 5
+print(is_even_number(my_number))
 print(is_whole_number(my_number))
+
+
